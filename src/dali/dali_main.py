08a28ad5bd37ff5e5b5c652e5ead08988c1977ac
eprbell <77937475+eprbell@users.noreply.github.com>
2022-03-06 18:10:38 -0800

    Bump version: 0.3.6 → 0.3.7

--- src/dali/dali_main.py
+++ src/dali/dali_main.py
@@ -42,7 +42,7 @@ from dali.ods_generator import generate_input_file
 from dali.out_transaction import OutTransaction
 from dali.transaction_resolver import DirectionTypeAndNotes, resolve_transactions
 
-_VERSION: str = "0.3.6"
+_VERSION: str = "0.3.7"
 
 
 def input_loader() -> None:
