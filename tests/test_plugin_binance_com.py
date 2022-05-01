ac42fea6e4e5263db94b432bf0d37da6db25a0a7
macanudo@yahoo.com <macanudo@yahoo.com>
2022-05-01 18:56:28 +0900

    Add test for trades

--- tests/test_plugin_binance_com.py
+++ tests/test_plugin_binance_com.py
@@ -140,8 +140,9 @@ class TestBinance:
 
         result = plugin.load()
 
-        # One completed Fiat Payment +
-        # 
+        # 1 completed Fiat Payment +
+        # 1 crypto Transfer +
+        # 1 fiat deposit = 3
         assert len(result) == 3
 
         fiat_in_transaction: InTransaction = result[0]
@@ -185,4 +186,175 @@ class TestBinance:
             username="user",
         )       
        
+        plugin.markets = [{'id':'ETHBTC'}]
+        mocker.patch.object(plugin.client, "fetch_my_trades").return_value = [
 
+                # Trade using BNB for fee payment
+                {
+                  'info':         {'sample':'data'},          # the original decoded JSON as is
+                  'id':           '12345-67890:09876/54321',  # string trade id
+                  'timestamp':    1502962946216,              # Unix timestamp in milliseconds
+                  'datetime':     '2017-08-17 12:42:48.000',  # ISO8601 datetime with milliseconds
+                  'symbol':       'ETH/BTC',                  # symbol
+                  'order':        '12345-67890:09876/54321',  # string order id or undefined/None/null
+                  'type':         'limit',                    # order type, 'market', 'limit' or undefined/None/null
+                  'side':         'buy',                      # direction of the trade, 'buy' or 'sell'
+                  'takerOrMaker': 'taker',                    # string, 'taker' or 'maker'
+                  'price':        0.06917684,                 # float price in quote currency
+                  'amount':       1.5,                        # amount of base currency
+                  'cost':         0.10376526,                 # total cost, `price * amount`,
+                  'fee':          {                           # provided by exchange or calculated by ccxt
+                      'cost':  0.0015,                        # float
+                      'currency': 'BNB',                      # usually base currency for buys, quote currency for sells
+                      'rate': 0.002,                          # the fee rate (if available)
+                  },
+                },
+
+                # Trade using the quote currency for fee payment
+                {
+                  'info':         {'sample':'data'},          # the original decoded JSON as is
+                  'id':           '12345-67890:09876/54321',  # string trade id
+                  'timestamp':    1502962946217,              # Unix timestamp in milliseconds
+                  'datetime':     '2017-08-17 12:42:48.000',  # ISO8601 datetime with milliseconds
+                  'symbol':       'ETH/BTC',                  # symbol
+                  'order':        '12345-67890:09876/54321',  # string order id or undefined/None/null
+                  'type':         'limit',                    # order type, 'market', 'limit' or undefined/None/null
+                  'side':         'buy',                      # direction of the trade, 'buy' or 'sell'
+                  'takerOrMaker': 'taker',                    # string, 'taker' or 'maker'
+                  'price':        0.06917684,                 # float price in quote currency
+                  'amount':       3,                          # amount of base currency
+                  'cost':         0.20753052,                 # total cost, `price * amount`,
+                  'fee':          {                           # provided by exchange or calculated by ccxt
+                      'cost':  0.0015,                        # float
+                      'currency': 'ETH',                      # usually base currency for buys, quote currency for sells
+                      'rate': 0.002,                          # the fee rate (if available)
+                  },
+                },
+
+                # Sell trade using the quote currency for fee payment
+                {
+                  'info':         {'sample':'data'},          # the original decoded JSON as is
+                  'id':           '12345-67890:09876/54321',  # string trade id
+                  'timestamp':    1502962946218,              # Unix timestamp in milliseconds
+                  'datetime':     '2017-08-17 12:42:48.000',  # ISO8601 datetime with milliseconds
+                  'symbol':       'ETH/BTC',                  # symbol
+                  'order':        '12345-67890:09876/54321',  # string order id or undefined/None/null
+                  'type':         'limit',                    # order type, 'market', 'limit' or undefined/None/null
+                  'side':         'sell',                      # direction of the trade, 'buy' or 'sell'
+                  'takerOrMaker': 'taker',                    # string, 'taker' or 'maker'
+                  'price':        0.06917684,                 # float price in quote currency
+                  'amount':       6,                          # amount of base currency
+                  'cost':         0.41506104,                 # total cost, `price * amount`,
+                  'fee':          {                           # provided by exchange or calculated by ccxt
+                      'cost':  0.0015,                        # float
+                      'currency': 'BTC',                      # usually base currency for buys, quote currency for sells
+                      'rate': 0.002,                          # the fee rate (if available)
+                  },
+                },
+            ]
+
+        # CCXT abstracts dust trades into regular trades, so no testing is necessary
+        mocker.patch.object(plugin.client, "fetch_my_dust_trades").return_value = []
+        mocker.patch.object(plugin, "_process_deposits").return_value = None
+        mocker.patch.object(plugin, "_process_gains").return_value = None
+
+        result = plugin.load()
+
+        # One Sell of quote asset (using BNB) +
+        # One Buy of base asset (using BNB) +
+        # One payment for fees in BNB
+        # One Sell of quote asset (for Buy order) +
+        # One Buy of base asset (for Buy order) +
+        # One Sell of quote asset (for Sell order) +
+        # One Buy of base asset (for Sell order) = 7
+        assert len(result) == 7
+
+        BNB_sell_transaction: OutTransaction = result[3]
+        BNB_buy_transaction: InTransaction = result[0]
+        BNB_fee_transaction: OutTransaction = result[4]
+        regular_sell: OutTransaction = result[5]
+        regular_buy: InTransaction = result[1]
+        sell_order_sell: OutTransaction = result[6]
+        sell_order_buy: InTransaction = result[2]
+
+        # Buy with BNB as fee payment
+        assert BNB_sell_transaction.asset == "BTC"
+        assert BNB_sell_transaction.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946216)
+        assert BNB_sell_transaction.transaction_type == Keyword.SELL.value
+        assert BNB_sell_transaction.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(BNB_sell_transaction.crypto_out_no_fee) == RP2Decimal("0.10376526")
+        assert RP2Decimal(BNB_sell_transaction.crypto_fee) == RP2Decimal("0")
+        assert RP2Decimal(BNB_sell_transaction.crypto_out_with_fee) == RP2Decimal("0.10376526")
+        assert BNB_sell_transaction.fiat_out_no_fee == None
+        assert BNB_sell_transaction.fiat_fee == None
+
+        assert BNB_buy_transaction.asset == "ETH"
+        assert BNB_buy_transaction.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946216)
+        assert BNB_buy_transaction.transaction_type == Keyword.BUY.value
+        assert BNB_buy_transaction.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(BNB_buy_transaction.crypto_in) == RP2Decimal("1.5")
+        assert RP2Decimal(BNB_buy_transaction.crypto_fee) == RP2Decimal("0")
+        assert BNB_buy_transaction.fiat_in_no_fee == None
+        assert BNB_buy_transaction.fiat_in_with_fee == None
+        assert BNB_buy_transaction.fiat_fee == None
+
+        assert BNB_fee_transaction.asset == "BNB"
+        assert BNB_fee_transaction.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946216)
+        assert BNB_fee_transaction.transaction_type == Keyword.FEE.value
+        assert BNB_fee_transaction.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(BNB_fee_transaction.crypto_out_no_fee) == RP2Decimal("0.0015")
+        assert RP2Decimal(BNB_fee_transaction.crypto_fee) == RP2Decimal("0")
+        assert RP2Decimal(BNB_fee_transaction.crypto_out_with_fee) == RP2Decimal("0.0015")
+        assert BNB_fee_transaction.fiat_out_no_fee == None
+        assert BNB_fee_transaction.fiat_fee == None
+
+        # Buy with base asset as fee payment
+        assert regular_sell.asset == "BTC"
+        assert regular_sell.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946217)
+        assert regular_sell.transaction_type == Keyword.SELL.value
+        assert regular_sell.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(regular_sell.crypto_out_no_fee) == RP2Decimal("0.20753052")
+        assert RP2Decimal(regular_sell.crypto_fee) == RP2Decimal("0")
+        assert RP2Decimal(regular_sell.crypto_out_with_fee) == RP2Decimal("0.20753052")
+        assert regular_sell.fiat_out_no_fee == None
+        assert regular_sell.fiat_fee == None  
+
+        assert regular_buy.asset == "ETH"
+        assert regular_buy.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946217)
+        assert regular_buy.transaction_type == Keyword.BUY.value
+        assert regular_buy.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(regular_buy.crypto_in) == RP2Decimal("3")
+        assert RP2Decimal(regular_buy.crypto_fee) == RP2Decimal("0.0015")
+        assert regular_buy.fiat_in_no_fee == None
+        assert regular_buy.fiat_in_with_fee == None
+        assert regular_buy.fiat_fee == None     
+
+        # Sell with quote asset as fee payment
+        assert sell_order_sell.asset == "ETH"
+        assert sell_order_sell.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946218)
+        assert sell_order_sell.transaction_type == Keyword.SELL.value
+        assert sell_order_sell.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(sell_order_sell.crypto_out_no_fee) == RP2Decimal("6")
+        assert RP2Decimal(sell_order_sell.crypto_fee) == RP2Decimal("0")
+        assert RP2Decimal(sell_order_sell.crypto_out_with_fee) == RP2Decimal("6")
+        assert sell_order_sell.fiat_out_no_fee == None
+        assert sell_order_sell.fiat_fee == None  
+
+        assert sell_order_buy.asset == "BTC"
+        assert sell_order_buy.timestamp == InputPlugin._rp2timestamp_from_ms_epoch(1502962946218)
+        assert sell_order_buy.transaction_type == Keyword.BUY.value
+        assert sell_order_buy.spot_price == Keyword.UNKNOWN.value
+        assert RP2Decimal(sell_order_buy.crypto_in) == RP2Decimal("0.41506104")
+        assert RP2Decimal(sell_order_buy.crypto_fee) == RP2Decimal("0.0015")
+        assert sell_order_buy.fiat_in_no_fee == None
+        assert sell_order_buy.fiat_in_with_fee == None
+        assert sell_order_buy.fiat_fee == None 
+
+    # pylint: disable=no-self-use
+    def test_gains(self, mocker: Any) -> None:
+        plugin = InputPlugin(
+            account_holder="tester",
+            api_key="a",
+            api_secret="b",
+            username="user",
+        ) 
