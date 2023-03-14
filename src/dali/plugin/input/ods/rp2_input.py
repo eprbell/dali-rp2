6000f5e977698850a1d40d92301dedf83cabb7ac
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2023-03-14 09:50:53 -0500

    - fixed mypy warnings

--- src/dali/plugin/input/ods/rp2_input.py
+++ src/dali/plugin/input/ods/rp2_input.py
@@ -15,6 +15,7 @@
 import logging
 from typing import List, Optional, cast
 
+from rp2.abstract_country import AbstractCountry
 from rp2.logger import create_logger
 from rp2.ods_parser import parse_ods, open_ods
 from rp2.input_data import InputData
@@ -23,7 +24,7 @@ from rp2.in_transaction import InTransaction as RP2InTransaction
 from rp2.intra_transaction import IntraTransaction as RP2IntraTransaction
 from rp2.out_transaction import OutTransaction as RP2OutTransaction
 
-from dali import configuration as config
+from dali import configuration
 from dali.abstract_input_plugin import AbstractInputPlugin
 from dali.abstract_transaction import AbstractTransaction
 from dali.in_transaction import InTransaction
@@ -48,85 +49,85 @@ class InputPlugin(AbstractInputPlugin):
     def load(self) -> List[AbstractTransaction]:
         result: List[AbstractTransaction] = []
 
-        configuration: Configuration = Configuration(
+        rp2_configuration: Configuration = Configuration(
             configuration_path=self.__configuration_path,
-            country=config.COUNTRY,
+            country=cast(AbstractCountry, configuration.COUNTRY),
             from_date=MIN_DATE,
             to_date=MAX_DATE,
         )
 
-        input_file_handle: object = open_ods(configuration=configuration, input_file_path=self.__input_file)
-        assets = sorted(list(configuration.assets))
+        input_file_handle: object = open_ods(configuration=rp2_configuration, input_file_path=self.__input_file)
+        assets = sorted(list(rp2_configuration.assets))
 
         for asset in assets:
             self.__logger.info("Processing %s", asset)
 
             input_data: InputData = parse_ods(
-                configuration=configuration,
+                configuration=rp2_configuration,
                 asset=asset,
                 input_file_handle=input_file_handle
             )
             self.__logger.debug("InputData object: %s", input_data)
             for asset_entry in input_data.unfiltered_in_transaction_set:
-                transaction: RP2InTransaction = cast(RP2InTransaction, asset_entry)
-                self.__logger.debug("Transaction: %s", str(transaction))
+                in_transaction: RP2InTransaction = cast(RP2InTransaction, asset_entry)
+                self.__logger.debug("Transaction: %s", str(in_transaction))
                 result.append(InTransaction(
                     plugin=self.__RP2_INPUT,
-                    unique_id=transaction.unique_id,
-                    raw_data=str(transaction),
-                    timestamp=str(transaction.timestamp),
-                    asset=transaction.asset,
-                    exchange=transaction.exchange,
-                    holder=transaction.holder,
-                    transaction_type=transaction.transaction_type.value,
-                    spot_price=str(transaction.spot_price),
-                    crypto_in=str(transaction.crypto_in),
-                    crypto_fee=str(transaction.crypto_fee) if transaction.crypto_fee else None,
-                    fiat_in_no_fee=str(transaction.fiat_in_no_fee) if transaction.fiat_in_no_fee else None,
-                    fiat_in_with_fee=str(transaction.fiat_in_with_fee) if transaction.fiat_in_with_fee else None,
-                    fiat_fee=str(transaction.fiat_fee) if transaction.fiat_fee else None,
-                    notes=str(transaction.notes) if transaction.notes else None
+                    unique_id=in_transaction.unique_id,
+                    raw_data=str(in_transaction),
+                    timestamp=str(in_transaction.timestamp),
+                    asset=in_transaction.asset,
+                    exchange=in_transaction.exchange,
+                    holder=in_transaction.holder,
+                    transaction_type=in_transaction.transaction_type.value,
+                    spot_price=str(in_transaction.spot_price),
+                    crypto_in=str(in_transaction.crypto_in),
+                    crypto_fee=str(in_transaction.crypto_fee) if in_transaction.crypto_fee else None,
+                    fiat_in_no_fee=str(in_transaction.fiat_in_no_fee) if in_transaction.fiat_in_no_fee else None,
+                    fiat_in_with_fee=str(in_transaction.fiat_in_with_fee) if in_transaction.fiat_in_with_fee else None,
+                    fiat_fee=str(in_transaction.fiat_fee) if in_transaction.fiat_fee else None,
+                    notes=str(in_transaction.notes) if in_transaction.notes else None
                 ))
 
             for asset_transfer in input_data.unfiltered_intra_transaction_set:
-                transaction: RP2IntraTransaction = cast(RP2IntraTransaction, asset_transfer)
-                self.__logger.debug("Transaction: %s", str(transaction))
+                intra_transaction: RP2IntraTransaction = cast(RP2IntraTransaction, asset_transfer)
+                self.__logger.debug("Transaction: %s", str(intra_transaction))
                 result.append(
                     IntraTransaction(
                         plugin=self.__RP2_INPUT,
-                        unique_id=transaction.unique_id,
-                        raw_data=str(transaction),
-                        timestamp=str(transaction.timestamp),
-                        asset=transaction.asset,
-                        from_exchange=transaction.from_exchange,
-                        from_holder=transaction.from_holder,
-                        to_exchange=transaction.to_exchange,
-                        to_holder=transaction.to_holder,
-                        spot_price=str(transaction.spot_price) if transaction.spot_price else None,
-                        crypto_sent=str(transaction.crypto_sent),
-                        crypto_received=str(transaction.crypto_received),
-                        notes=str(transaction.notes) if transaction.notes else None
+                        unique_id=intra_transaction.unique_id,
+                        raw_data=str(intra_transaction),
+                        timestamp=str(intra_transaction.timestamp),
+                        asset=intra_transaction.asset,
+                        from_exchange=intra_transaction.from_exchange,
+                        from_holder=intra_transaction.from_holder,
+                        to_exchange=intra_transaction.to_exchange,
+                        to_holder=intra_transaction.to_holder,
+                        spot_price=str(intra_transaction.spot_price) if intra_transaction.spot_price else None,
+                        crypto_sent=str(intra_transaction.crypto_sent),
+                        crypto_received=str(intra_transaction.crypto_received),
+                        notes=str(intra_transaction.notes) if intra_transaction.notes else None
                     ))
 
             for asset_exit in input_data.unfiltered_out_transaction_set:
-                transaction: RP2OutTransaction = cast(RP2OutTransaction, asset_exit)
-                self.__logger.debug("Transaction: %s", str(transaction))
+                out_transaction: RP2OutTransaction = cast(RP2OutTransaction, asset_exit)
+                self.__logger.debug("Transaction: %s", str(out_transaction))
                 result.append(
                     OutTransaction(
                         plugin=self.__RP2_INPUT,
-                        unique_id=transaction.unique_id,
-                        raw_data=str(transaction),
-                        timestamp=str(transaction.timestamp),
-                        asset=transaction.asset,
-                        exchange=transaction.exchange,
-                        holder=transaction.holder,
-                        transaction_type=transaction.transaction_type.value,
-                        spot_price=str(transaction.spot_price),
-                        crypto_out_no_fee=str(transaction.crypto_out_no_fee),
-                        crypto_fee=str(transaction.crypto_fee),
-                        crypto_out_with_fee=str(transaction.crypto_out_with_fee),
-                        fiat_out_no_fee=str(transaction.fiat_out_no_fee) if transaction.fiat_out_no_fee else None,
-                        fiat_fee=str(transaction.fiat_fee) if transaction.fiat_fee else None,
-                        notes=str(transaction.notes) if transaction.notes else None
+                        unique_id=out_transaction.unique_id,
+                        raw_data=str(out_transaction),
+                        timestamp=str(out_transaction.timestamp),
+                        asset=out_transaction.asset,
+                        exchange=out_transaction.exchange,
+                        holder=out_transaction.holder,
+                        transaction_type=out_transaction.transaction_type.value,
+                        spot_price=str(out_transaction.spot_price),
+                        crypto_out_no_fee=str(out_transaction.crypto_out_no_fee),
+                        crypto_fee=str(out_transaction.crypto_fee),
+                        crypto_out_with_fee=str(out_transaction.crypto_out_with_fee),
+                        fiat_out_no_fee=str(out_transaction.fiat_out_no_fee) if out_transaction.fiat_out_no_fee else None,
+                        fiat_fee=str(out_transaction.fiat_fee) if out_transaction.fiat_fee else None,
+                        notes=str(out_transaction.notes) if out_transaction.notes else None
                     ))
         return result
