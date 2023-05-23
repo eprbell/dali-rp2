cb03ad5b0f1552c53fd71411c04f5dd2684548c2
Christopher Buckley <topherbuckley@gmail.com>
2023-05-23 15:37:06 -0400

    fixing convention warnings

--- src/dali/transaction_item.py
+++ src/dali/transaction_item.py
@@ -17,8 +17,9 @@ from enum import Enum
 from dali.configuration import Keyword
 
 
-class Transaction_Item:
-    def __init__(self, disallow_empty: bool, disallow_unknown: bool, transaction_item_type: Keyword) -> None:
+class TransactionItem:
+    def __init__(self, disallow_empty: bool, disallow_unknown: bool,
+                 transaction_item_type: Keyword) -> None:
         self.__disallow_empty: bool = disallow_empty
         self.__disallow_unknown: bool = disallow_unknown
         self.__transaction_item_type: Keyword = transaction_item_type
@@ -36,12 +37,12 @@ class Transaction_Item:
         return self.__transaction_item_type
 
 
-class TRANSACTION_ITEM_TYPE(Enum):
-    __members__: Transaction_Item
-    PLUGIN = Transaction_Item(True, True, Keyword.PLUGIN)
-    UNIQUE_ID = Transaction_Item(True, False, Keyword.UNIQUE_ID)
-    RAW_DATA = Transaction_Item(True, True, Keyword.RAW_DATA)
-    TIMESTAMP = Transaction_Item(True, True, Keyword.TIMESTAMP)
-    ASSET = Transaction_Item(True, True, Keyword.ASSET)
-    NOTES = Transaction_Item(False, True, Keyword.NOTES)
-    FIAT_TICKER = Transaction_Item(True, True, Keyword.FIAT_TICKER)
+class TransactionItemType(Enum):
+    __members__: TransactionItem
+    PLUGIN = TransactionItem(True, True, Keyword.PLUGIN)
+    UNIQUE_ID = TransactionItem(True, False, Keyword.UNIQUE_ID)
+    RAW_DATA = TransactionItem(True, True, Keyword.RAW_DATA)
+    TIMESTAMP = TransactionItem(True, True, Keyword.TIMESTAMP)
+    ASSET = TransactionItem(True, True, Keyword.ASSET)
+    NOTES = TransactionItem(False, True, Keyword.NOTES)
+    FIAT_TICKER = TransactionItem(True, True, Keyword.FIAT_TICKER)
