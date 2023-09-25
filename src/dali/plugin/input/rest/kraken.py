98ce2bf4e04dd85a08fdfdcf95c4a6e600df3570
Neal Chambers <macanudo@yahoo.com>
2023-09-25 12:59:59 +0900

    Fix Formatting

--- src/dali/plugin/input/rest/kraken.py
+++ src/dali/plugin/input/rest/kraken.py
@@ -128,9 +128,11 @@ class InputPlugin(AbstractCcxtInputPlugin):
 
         for markets in self._client.markets_by_id.values():
             if not isinstance(markets, list):  # type: ignore
-                exc_str = f"Expected List from Kraken CCXT Exchange, got {type(markets)} instead. " \
-                          f"Incompatible CCXT library - make sure to follow Dali setup instructions " \
-                          f"to install appropriate versions of dependencies."
+                exc_str = (
+                    f"Expected List from Kraken CCXT Exchange, got {type(markets)} instead. "
+                    f"Incompatible CCXT library - make sure to follow Dali setup instructions "
+                    f"to install appropriate versions of dependencies."
+                )
                 raise RP2RuntimeError(exc_str)
 
             for market in markets:  # type: ignore
@@ -140,12 +142,13 @@ class InputPlugin(AbstractCcxtInputPlugin):
                 # multiple markets for a market ID. This plugin is designed for one BASE symbol to a BASE_ID.
                 # If this condition occurs, it indicates the Exchange has changed its response to the
                 # API call. Please report the issue to the DALI-RP2 developers.
-                if base_id in self.base_id_to_base and \
-                    market[_BASE] != self.base_id_to_base[base_id]:
-                    exc_str = f"Unsupported BASE for BASE_ID. Please open an issue at {self.ISSUES_URL}. " \
-                              f"A Kraken market's BASE differs with another BASE for the same BASE_ID. " \
-                              f"BASE_ID={base_id}, discovered BASE={market[_BASE]}, " \
-                              f"previous cached base={self.base_id_to_base[base_id]}"
+                if base_id in self.base_id_to_base and market[_BASE] != self.base_id_to_base[base_id]:
+                    exc_str = (
+                        f"Unsupported BASE for BASE_ID. Please open an issue at {self.ISSUES_URL}. "
+                        f"A Kraken market's BASE differs with another BASE for the same BASE_ID. "
+                        f"BASE_ID={base_id}, discovered BASE={market[_BASE]}, "
+                        f"previous cached base={self.base_id_to_base[base_id]}"
+                    )
                     raise RP2RuntimeError(exc_str)
                 self.base_id_to_base.update({market[_BASE_ID]: market[_BASE]})
 
@@ -260,9 +263,11 @@ class InputPlugin(AbstractCcxtInputPlugin):
                 # the API call. Please report the issue to the DALI-RP2 developers.
                 if len(markets) > 1:
                     possible_quotes = [market[_QUOTE] for market in markets]
-                    exc_str = f"Multiple quotes for pair. Please open an issue at {self.ISSUES_URL}. " \
-                              f"Which quote to use for {trade_history[record[_REFID]][_PAIR]} market? " \
-                              f"Possible quotes={possible_quotes}"
+                    exc_str = (
+                        f"Multiple quotes for pair. Please open an issue at {self.ISSUES_URL}. "
+                        f"Which quote to use for {trade_history[record[_REFID]][_PAIR]} market? "
+                        f"Possible quotes={possible_quotes}"
+                    )
                     raise RP2RuntimeError(exc_str)
                 asset_quote: str = markets[0][_QUOTE]
                 is_quote_asset_fiat: bool = asset_quote in _KRAKEN_FIAT_LIST
