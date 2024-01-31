c345b9d00d7ba18326725dd07a7ea9ec11078a01
Neal Chambers <macanudo@yahoo.com>
2024-01-31 17:21:39 +0900

    Add Tests for Kraken CSV Plugin

--- tests/test_plugin_kraken_csv_download.py
+++ tests/test_plugin_kraken_csv_download.py
@@ -14,7 +14,7 @@
 
 # pylint: disable=protected-access
 
-from datetime import datetime
+from datetime import datetime, timezone
 from os import listdir, makedirs, path, remove, unlink
 from typing import Any, List, Optional
 
@@ -22,10 +22,32 @@ from rp2.rp2_decimal import RP2Decimal
 from rp2.rp2_error import RP2RuntimeError
 
 from dali.cache import CACHE_DIR
+from dali.configuration import Keyword
 from dali.historical_bar import HistoricalBar
+from dali.in_transaction import InTransaction
 from dali.plugin.pair_converter.csv.kraken import Kraken
+from dali.transaction_manifest import TransactionManifest
 
 _CACHE_DIRECTORY: str = "output/kraken_test"
+
+# Fake Transaction
+FAKE_TRANSACTION: InTransaction = InTransaction(
+    plugin="Plugin",
+    unique_id=Keyword.UNKNOWN.value,
+    raw_data="raw",
+    timestamp=datetime.fromtimestamp(1504541500, timezone.utc).strftime("%Y-%m-%d %H:%M:%S%z"),
+    asset="BTC",
+    exchange="Kraken",
+    holder="test",
+    transaction_type=Keyword.BUY.value,
+    spot_price=Keyword.UNKNOWN.value,
+    crypto_in="1",
+    crypto_fee=None,
+    fiat_in_no_fee=None,
+    fiat_in_with_fee=None,
+    fiat_fee=None,
+    notes="notes",
+)
 
 
 class TestKrakenCsvDownload:
@@ -48,8 +70,8 @@ class TestKrakenCsvDownload:
         mocker.patch.object(kraken_csv, "_Kraken__CACHE_DIRECTORY", _CACHE_DIRECTORY)
         if not path.exists(_CACHE_DIRECTORY):
             makedirs(_CACHE_DIRECTORY)
-        with open("input/USD_OHLCVT_test.zip", "rb") as file:
-            mocker.patch.object(kraken_csv, "_google_file_to_bytes").return_value = file.read()
+
+        mocker.patch.object(kraken_csv, "_Kraken__UNIFIED_CSV_FILE", "input/USD_OHLCVT_test.zip")
 
         test_bar: Optional[HistoricalBar] = kraken_csv.find_historical_bar("USDT", "USD", datetime.fromtimestamp(1601856000))
         files: List[str] = listdir(_CACHE_DIRECTORY)
